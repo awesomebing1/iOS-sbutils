2fd713c81f938043e376ad1b955c7296bf8a3534
John Smith <awesomebing1@gmail.com>
2015-12-12 08:55:55 -0600

    Oops...
    I can’t fix this due to a linker error. I’m sure that there’s an easier
    way to do it but I can’t fix it. If you can fix it, please submit a
    pull request!

--- sburlschemes.c
+++ sburlschemes.c
@@ -1,15 +1,16 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <stdio.h>
 
-CFArrayRef SBSCopyPublicURLSchemes();
+// CFArrayRef SBSCopyPublicURLSchemes();
 
 int main() {
-    char buf[1024];
-    CFArrayRef ary = SBSCopyPublicURLSchemes();
-    CFIndex i;
-    for(i = 0; i < CFArrayGetCount(ary); i++) {
-        CFStringGetCString(CFArrayGetValueAtIndex(ary, i), buf, sizeof(buf), kCFStringEncodingUTF8);
-        printf("%s\n", buf);
-    }
+    // char buf[1024];
+    // CFArrayRef ary = SBSCopyPublicURLSchemes();
+    // CFIndex i;
+    // for(i = 0; i < CFArrayGetCount(ary); i++) {
+    //     CFStringGetCString(CFArrayGetValueAtIndex(ary, i), buf, sizeof(buf), kCFStringEncodingUTF8);
+    //     printf("%s\n", buf);
+    // }
+    printf("%s\n", "On 12/12/15, I commented out everything because I couldn't fix it. \n Sorry.\n");
     return 0;
 }
